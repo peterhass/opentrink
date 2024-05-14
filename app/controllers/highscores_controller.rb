557412fe18be9cbe002665ce1e5830b056839b90
Peter Haßler <peter@hassler.in>
2024-05-14 23:49:25 +0200

    Hide users without any score from the highscores board

--- app/controllers/highscores_controller.rb
+++ app/controllers/highscores_controller.rb
@@ -1,5 +1,5 @@
 class HighscoresController < ApplicationController
   def show 
-    @participants = Participant.by_consumptions_desc.limit(10)
+    @participants = Participant.by_consumptions_desc.limit(10).reject { |p| p.consumptions_count.nil? }
   end
 end
