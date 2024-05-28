e4ed050e5b893290dc6eef24df7983d2b790b06d
Peter Haßler <peter@hassler.in>
2024-05-29 00:46:10 +0200

    Add bar invitation creation link

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -4,4 +4,17 @@ class InvitationsController < ApplicationController
   def index
     @invitations = Invitation.all.newest_first
   end
+
+  def create
+    @invitation = Invitation.new(invitation_params)
+
+    @invitation.save!
+    redirect_to action: :index
+  end
+
+  private
+
+  def invitation_params
+    params.require(:invitation).permit(:role)
+  end
 end
