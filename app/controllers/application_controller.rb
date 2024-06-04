92f6cc9658b92fdba0e08c9f8d875e70846b10b0
Peter Haßler <peter@hassler.in>
2024-06-04 13:59:35 +0200

    Persist participants in session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,17 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def current_participant=(participant)
+    session[:participant_id] = participant.id
+  end
+
+  helper_method :current_participant
+  def current_participant
+    return nil unless session[:participant_id]
+
+    @current_participant ||= Participant.find_by(id: session[:participant_id])
+  end
+
   def require_admin
     return if current_user&.activated? && current_user&.admin?
 
--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,12 +1,13 @@
 class ParticipantsController < ApplicationController
-  before_action :require_bar, except: %i[new create]
+  before_action :require_bar, except: %i[new create show]
 
   def index
     @participants = Participant.newest_first
   end
 
   def show
-    @participant = Participant.find_by(uid: params[:id])
+    @participant = Participant.find_by!(uid: params[:id])
+    raise 'not allowed' unless @participant.id == current_participant&.id
   end
 
   def new
@@ -17,6 +18,7 @@ class ParticipantsController < ApplicationController
     @participant = Participant.new(params.require(:participant).permit(:name))
 
     if @participant.save
+      self.current_participant = @participant
       redirect_to action: :show, id: @participant.uid
     else
       render :new, status: :unprocessable_entity
