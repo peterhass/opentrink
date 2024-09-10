f3e090b49487d52edb3b1749934cd4139afa8ab2
Peter Haßler <peter@hassler.in>
2024-09-10 23:30:22 +0200

    Fix session cookie to survive for the whole lifetime of the event

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,2 @@
-Rails.application.config.session_store :active_record_store, key: 'opentrink_session'
+Rails.application.config.session_store :active_record_store, key: 'opentrink_session', expire_after: 5.months
 ActiveRecord::SessionStore::Session.serializer = :json
